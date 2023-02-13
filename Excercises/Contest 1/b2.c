b54e2ede854032ce3dc66a422e18bc971bb535dd
ngocnguyen1998 <nvngocembedded@gmail.com>
2023-02-13 07:53:01 +0700

    add contest 1/b2

---
+++ Excercises/Contest 1/b2.c
@@ -0,0 +1,25 @@
+/*
+------Problem------
+Cho 2 số x, y. Nhiệm vụ của bạn là tính x ^ y
+
+------Input Format------
+1 dòng chứa 2 số nguyên dương x, y viết cách nhau một dấu cách
+
+------Constraints------
+1<=x,y<=12;
+
+------Output Format------
+In ra x^y, nếu x^y có phần thập phân thì in ra 2 số sau dấu phẩy, nếu không có phần thập phân thì không cần in.
+
+------Sample Input 0------
+2 2
+
+------Sample Output 0------
+4
+
+------Sample Input 1------
+4 1
+
+------Sample Output 1------
+4
+*/
